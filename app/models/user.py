55e69a07f1d066c59a07ac40ba4eac712f0661a3
Antonio Hickey <antoniohickey99@gmail.com>
2022-03-24 12:30:50 -0400

    feat: create model to represent a user

---
+++ app/models/user.py
@@ -0,0 +1,46 @@
+from datetime import datetime, timezone
+from typing import Any, DefaultDict
+
+from pynamodb.attributes import (MapAttribute, UnicodeAttribute,
+                                 UTCDateTimeAttribute)
+from pynamodb.indexes import GlobalSecondaryIndex
+from pynamodb.models import Model
+from pynamodb_attributes import IntegerAttribute
+
+from app.models._meta import pynamodb_index_meta, pynamodb_table_meta
+
+
+class UsernameIndex(GlobalSecondaryIndex):
+    """Search on usernames."""
+    Meta = pynamodb_index_meta(hash_key="username", range_key=None)
+    username_search = UnicodeAttribute(hash_key=True)
+
+
+class User(Model):
+    """Table representing the user."""
+    Meta = pynamodb_table_meta("relay-users")
+
+    # User attributes
+    id = IntegerAttribute(hash_key=True)
+    name = UnicodeAttribute(null=True)
+    password_hashed = UnicodeAttribute(null=True)
+    rsa_pub_key_n = UnicodeAttribute(null=True)
+    rsa_pub_key_e = UnicodeAttribute(null=True)
+    contacts = MapAttribute(default=DefaultDict)
+    created_ts = UTCDateTimeAttribute(null=True)
+    last_update_ts = UTCDateTimeAttribute(null=True)
+
+    # User search indexes
+    username_search_index = UsernameIndex()
+
+    def save(self, *args: Any, **kwargs) -> Any:
+        """Save user instance"""
+        self.last_updated_ts = datetime.now(timezone.utc)
+        return super().save(*args, **kwargs)
+
+    def as_dict(self) -> dict:
+        """User as hashmap."""
+        hashmap = {}
+        for key in self.attribute_values:
+            hashmap[key] = self.__getattribute__(key)
+        return hashmap
