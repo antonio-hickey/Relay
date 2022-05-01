881c2600e3844b28ba0fe37d543e91acb84c792d
Antonio Hickey <antoniohickey99@gmail.com>
2022-04-30 23:52:22 -0400

    feat(web app): module containing link variables

---
+++ app/static/js/common/links.js
@@ -0,0 +1,10 @@
+const base = "http://localhost:4200"
+const signInPage = base + "/web-app/sign-in";
+
+// This page does NOT exist yet!
+const comsPage = base + "/web-app/communication";
+
+
+export {
+  signInPage, base, comsPage,
+};
